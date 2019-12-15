c14f1d06559f223465e4d04a5908d326cd631dda
zf1990 <fang1534@stthomas.edu>
2019-12-15 16:02:20 -0600

    ZHou editted Sales

--- src/MainMethod1.java
+++ src/MainMethod1.java
@@ -1,8 +1,9 @@
 
 public class MainMethod1 {
 	public static void main(String[] args) {
-		Menu menu1 = new Menu();
-		menu1.PrintHeader();
+//		Menu menu1 = new Menu();
+//		menu1.PrintHeader();
+		System.out.println("hello");
 	}
 
 }
